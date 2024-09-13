d1ecb1dade3554ca930a7bca69b9a65c6eb4132f
Edgardramos33 <edgardramoz15@gmail.com>
2024-09-13 15:55:38 +0800

    added Js for all websites

---
+++ web.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  const url = window.location.pathname;
+  
+  if (url.includes('index.html')) {
+      alert("Welcome to the home page!");
+  } else if (url.includes('Hobbies.html')) {
+      alert("Hello, this is the hobbies page!");
+  } else if (url.includes('Gallery.html')) {
+      alert("Check out my gallery!");
+  }
+};
