2b5147ae1e8774c95a204795fc1f0da504423622
Edgardramos33 <edgardramoz15@gmail.com>
2024-09-14 13:36:02 +0800

    Update web.js for alert

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ window.onload = function() {
   
   if (url.includes('index.html')) {
       alert("Welcome to the home page!");
+      alert("if you can't see background image UC blocked it")
   } else if (url.includes('Hobbies.html')) {
       alert("Hello, this is the hobbies page!");
   } else if (url.includes('Gallery.html')) {
